b4247ead32a9a474fda921feb2a8c45b6ff14576
Gabriel Godoy <gabe.profe@gmail.com>
2023-02-01 23:01:04 -0300

    dom injector / property decorator

--- app/src/decorators/dom-injector.ts
+++ app/src/decorators/dom-injector.ts
@@ -1,25 +1,21 @@
 export function domInjector(seletor: string) {
-    
-    return function(target: any, propertyKey: string) {
-        console.log(`Modificando protype ${target.constructor.name}
-             e adicionando getter para a propriedade ${propertyKey}`);
 
-        let elemento: HTMLElement;
+    return function (target: any, propertyKey: string) {
 
-        const getter = function() {
-            if (!elemento) {
-                elemento = <HTMLElement>document.querySelector(seletor);
-                console.log(`buscando elemento do DOM com o seletor 
-                 ${seletor} para injetar em ${propertyKey}`);
-            }
-            
+        console.log(`Modificando prototype ${target.constructor.name} e adicionando getter para a propriedade ${propertyKey}`);
+
+        const getter = function () {
+            const elemento = document.querySelector(seletor)
+            console.log(`Buscando elemento do DOM com o seletor 
+                ${seletor} para injetar em ${propertyKey}`);
             return elemento;
         }
 
         Object.defineProperty(
-            target, 
-            propertyKey, 
+            target,
+            propertyKey,
             { get: getter }
-        );
+        )
+
     }
 }
